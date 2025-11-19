b2f86576714187d361d0f98e5ddbfc245e02e20e
zkssk0812 <94787318+zkssk0812@users.noreply.github.com>
2025-11-19 13:18:54 +0900

    Add files via upload

---
+++ ServerTermProject/GameServer.cpp
@@ -0,0 +1,162 @@
+#include "GameServer.h"
+#include <stdio.h>
+
+
+
+GameServer::GameServer(int port) : listen_sock(socket(AF_INET, SOCK_STREAM, 0)), Port(port) {
+
+	if (listen_sock == INVALID_SOCKET)
+		err_quit("socket()");
+
+	sockaddr_in serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons(Port);
+
+	retval = bind(listen_sock, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("bind()");
+
+	retval = listen(listen_sock, SOMAXCONN);
+	if (retval == SOCKET_ERROR) err_quit("listen()");
+}
+
+void err_quit(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	MessageBoxA(NULL, (const char*)lpMsgBuf, msg, MB_ICONERROR);
+	LocalFree(lpMsgBuf);
+	exit(1);
+}
+
+void err_display(const char* msg)
+{
+	LPVOID lpMsgBuf;
+	FormatMessageA(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, WSAGetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(char*)&lpMsgBuf, 0, NULL);
+	printf("[%s] %s\n", msg, (char*)lpMsgBuf);
+	LocalFree(lpMsgBuf);
+}
+
+void GameServer::ready() {
+
+	static int PlayerCount = 0;
+	
+	sockaddr_in clientaddr;
+	int addrlen = sizeof(clientaddr);
+	while (true) {
+		Socket client_sock{ accept(listen_sock, reinterpret_cast<sockaddr*>(&clientaddr), &addrlen) };
+		if (client_sock == INVALID_SOCKET) {
+			err_display("accept()");
+			continue;
+		}
+	
+
+		char addr[INET_ADDRSTRLEN];
+		inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+		printf("[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d", addr, ntohs(clientaddr.sin_port));
+
+		
+
+		char buf[4]{};
+		memcpy(buf, &PlayerCount, sizeof(PlayerCount));
+
+		retval = send(client_sock, buf, sizeof(PlayerCount), 0);
+
+		std::shared_ptr<Socket> clientSocketPtr = std::make_shared<Socket>(std::move(client_sock));
+
+		ThreadArgs* args = new ThreadArgs{ this, clientSocketPtr };
+
+		EnterCriticalSection(&m_clientListCS);
+		m_clientList[PlayerCount] = clientSocketPtr;
+		LeaveCriticalSection(&m_clientListCS);
+
+		++PlayerCount;
+
+		hThread[PlayerCount] = CreateThread(NULL, 0, ClientThread, args, 0, NULL);
+
+		
+
+		if (PlayerCount >= 2) break;
+		
+	}
+
+	this->run();
+
+}
+
+DWORD WINAPI GameServer::ClientThread(LPVOID arg) {
+	ThreadArgs* args = static_cast<ThreadArgs*>(arg);
+	while (true) {
+		
+	}
+	return 0;
+}
+
+
+void GameServer::run() {
+	
+
+	hThread[0] = CreateThread(NULL, 0, GameLogic, this, 0, NULL);
+}
+
+DWORD WINAPI GameServer::GameLogic(LPVOID arg) {
+
+	while (true) {
+		GameServer* server = static_cast<GameServer*>(arg);
+		BaseTask* bs = server->threadQueue.Pop();
+		
+		if (bs->taskType == 100) {
+			
+			server->HandleCardRequest(bs);
+		}
+		if (bs->taskType == 101) {
+			server->HandleCollisionRequest(bs);
+		}
+	}
+	
+	return 0;
+
+}
+
+void GameServer::HandleCardRequest(BaseTask* arg) {
+	int playerNum{};
+	for (int i = 0; i < 2; ++i) {
+		EnterCriticalSection(&m_clientListCS);
+		if (arg->clientSocket == m_clientList[i]) playerNum = i;
+		LeaveCriticalSection(&m_clientListCS);
+	}
+	
+	char buf[DATASIZE];
+	memcpy(buf, &playerNum, sizeof(playerNum));
+
+	LeaveCriticalSection(&m_clientListCS);
+	for (int i = 0; i < 2; ++i) {
+		EnterCriticalSection(&m_clientListCS);
+		send(*m_clientList[i], buf, sizeof(playerNum), 0);
+		send(*m_clientList[i], arg->data, sizeof(arg->data), 0);
+		LeaveCriticalSection(&m_clientListCS);
+	}
+}
+
+void GameServer::HandleCollisionRequest(BaseTask* arg) {
+	int playerNum{};
+	for (int i = 0; i < 2; ++i) {
+		EnterCriticalSection(&m_clientListCS);
+		if (arg->clientSocket == m_clientList[i]) playerNum = i;
+		LeaveCriticalSection(&m_clientListCS);
+	}
+
+	char buf[DATASIZE];
+	memcpy(buf, &playerNum, sizeof(playerNum));
+}
+
+
